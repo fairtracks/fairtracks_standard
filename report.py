6823159df918580a987927fc2e8161ef20c07c57
dtitov <dmytro.titov@gmail.com>
2020-06-16 16:46:17 +0200

    Split into BLUEPRINT minimal and augmented validation

--- report.py
+++ report.py
@@ -1,5 +1,4 @@
 import re
 
 lines = tuple(open('validation.log', 'r'))
-print(*[''] + [' [{}] {}'.format(*u) for u in enumerate(
-    sorted(set(re.sub('-=> (.*) <=-', '-=> Y <=-', re.sub('/\\d+/', '/X/', e)) for e in lines)))])
+print(*[''] + [' [{}] {}'.format(*u) for u in enumerate(sorted(set(re.sub('/\\d+/', '/X/', e) for e in lines)))])
