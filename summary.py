9e18738e2afbc9c55e1258223a4ed73b6ce4a17e
dtitov <dmytro.titov@gmail.com>
2020-06-16 18:03:39 +0200

    Better formatting

--- summary.py
+++ summary.py
@@ -7,4 +7,5 @@ print('\tTotal errors: ', len(lines))
 print('\tMerged errors: ')
 substituted_lines = list(re.sub('/\\d+/', '/X/', e) for e in lines)
 errors = {i: substituted_lines.count(i) for i in substituted_lines}
-print('\n'.join('\t\t[{}] {}'.format(*e) for e in enumerate(errors.items())))
+print('\n'.join('\t[{}] {}'.format(*e) for e in enumerate(errors.items())))
+print('\n')
