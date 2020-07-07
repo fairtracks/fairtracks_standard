160776c5358aad1ff1ebd060ddb089055696328f
Sveinung Gundersen <sveinugu@gmail.com>
2020-07-07 17:58:12 +0200

    Removed validate script due to updates in fairtracks_validate. Now handles validation exit codes gracefully. Updated summary.py

--- scripts/python/summary.py
+++ scripts/python/summary.py
@@ -1,11 +1,22 @@
 import re
 
-print('Short validation summary')
+
+all_lines = open('validation.log', 'r').readlines()
 pattern = re.compile('Path: /[^\n]*')
-lines = re.findall(pattern, open('validation.log', 'r').read())
-print('\tTotal errors: ', len(lines))
-print('\tMerged errors: ')
-substituted_lines = list(re.sub('/\\d+', '/X', e) for e in lines)
+error_lines = [_ for _ in all_lines if re.search(pattern, _)]
+substituted_lines = list(re.sub('/\\d+', '/X', e) for e in error_lines)
 errors = {i: substituted_lines.count(i) for i in substituted_lines}
-print('\n'.join('\t[{}] {}'.format(*e) for e in enumerate(errors.items())))
-print('\n')
+
+print('SHORT_VALIDATION_SUMMARY')
+print('------------------------')
+print('Validation Stats (from fairGTrackJsonValidate):')
+
+for stat_line in all_lines[-3:]:
+    print(stat_line, end='')
+
+print('\nSummary (from fairtracks_standard):')
+print('\tTotal errors: ', len(error_lines))
+if substituted_lines:
+    print('\tMerged errors: ')
+    print('\n'.join('\t[{}] {}'.format(i, e) for i, e in enumerate(errors.items())))
+    print('\n')
